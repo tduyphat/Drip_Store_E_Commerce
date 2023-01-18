2ca689097ecaca1bde240a6e4305cea4c605f83b
unknown <tranduyphat080300@gmail.com>
2023-01-18 16:59:15 +0200

    add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import Directory from './components/directory/directory.compnent';
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import SignIn from "./routes/sign-in/sign-in.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+
+const Shop = () => {
+  return <h1>I AM THE SHOP</h1>
+}
 
 const App = () => {
-
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://images.unsplash.com/photo-1523380744952-b7e00e6e2ffa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://images.unsplash.com/photo-1604644401890-0bd678c83788?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://images.unsplash.com/photo-1538329972958-465d6d2144ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://images.unsplash.com/photo-1542059765-8a5aff6c0dc7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-    }
-  ]
   return (
-    <Directory categories={categories} />
-  );
+    <Routes>
+      <Route path='/' element={<Navigation />} >
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,42 @@
+import { Outlet } from 'react-router-dom';
+import Directory from '../../components/directory/directory.compnent';
+
+const Home = () => {
+
+    const categories = [
+        {
+            "id": 1,
+            "title": "hats",
+            "imageUrl": "https://images.unsplash.com/photo-1523380744952-b7e00e6e2ffa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        },
+        {
+            "id": 2,
+            "title": "jackets",
+            "imageUrl": "https://images.unsplash.com/photo-1604644401890-0bd678c83788?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        },
+        {
+            "id": 3,
+            "title": "sneakers",
+            "imageUrl": "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        },
+        {
+            "id": 4,
+            "title": "womens",
+            "imageUrl": "https://images.unsplash.com/photo-1538329972958-465d6d2144ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        },
+        {
+            "id": 5,
+            "title": "mens",
+            "imageUrl": "https://images.unsplash.com/photo-1542059765-8a5aff6c0dc7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        }
+    ]
+    return (
+        <div>
+            <Outlet />
+            <Directory categories={categories} />
+        </div>
+
+    );
+}
+
+export default Home;
