8f50bae25612b1ad7461064c56710897436fd81b
unknown <tranduyphat080300@gmail.com>
2023-01-24 23:06:28 +0200

    put clothing data to Firestore, categorized items

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.scss';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
-import { ProductProvider } from './contexts/products.context';
+import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 
 import reportWebVitals from './reportWebVitals';
@@ -16,9 +16,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductProvider>
+        <CategoriesProvider>
           <CartProvider><App /></CartProvider>
-        </ProductProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
